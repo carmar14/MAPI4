ee20bdb5ee558a97633d2d9203400a9c1980892c
carmar14 <carmar141414@hotmail.com>
2026-02-22 10:58:26 -0500

    Entrenamiento de la LNN

--- liquid neural network/entrenamiento_lnn.py
+++ liquid neural network/entrenamiento_lnn.py
@@ -5,6 +5,7 @@ import torch
 from componentes import Tanque
 from simulacion import SimulacionNivel
 import numpy as np
+import matplotlib.pyplot as plt
 
 # --- 1. Configuración de entrada (Escalones) ---
 q_in_signal = np.repeat([0.8, 1.2, 0.8, 2.0, 1.5], 700) # Amplitudes variables
@@ -15,6 +16,8 @@ sim = SimulacionNivel(modelo_tanque=mi_tanque, dt=0.01, h0=0.0)
 
 # --- 3. Resultados simulacion ---
 time, niveles = sim.ejecutar(q_in_signal)
+h = niveles
+#----modelo de lnn-----
 model = LiquidTank(1, 20)
 optimizer = optim.Adam(model.parameters(), lr=0.01)
 criterion = nn.MSELoss()
@@ -31,6 +34,9 @@ for epoch in range(epochs):
     for i in range(len(time) - 1):
         q = torch.tensor([[q_in_signal[i]]], dtype=torch.float32)
         target = torch.tensor([[h[i + 1]]], dtype=torch.float32)
+
+        # IMPORTANTE: cortar el grafo
+        h_state = h_state.detach()
 
         pred, h_state = model(q, h_state)
 
--- liquid neural network/lnn.py
+++ liquid neural network/lnn.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
 
-
-
 class LiquidTank(nn.Module):
     def __init__(self, input_size, hidden_size):
         super().__init__()
